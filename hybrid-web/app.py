d3c90252cf6a0a0c7269b7cf8d8c4d4e8bc60c74
tjrosset <tjrosset@buffalo.edu>
2020-12-07 12:17:45 -0500

    Added News
    News is now added to the results page. Still buggy, needs work

--- hybrid-web/app.py
+++ hybrid-web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,redirect,url_for,request
 from urllib.request import urlopen
-from googlesearch import search
+from GoogleNews import GoogleNews
 import pysolr
 
 app = Flask(__name__)
@@ -13,7 +13,6 @@ def home():
         c = "Select"
         d = "Select"
         e = "Select"
-        print('submitted')
         return redirect(url_for("result", a=a, b=b, c=c, d=d))
     else:
         return render_template("home.html")
@@ -25,13 +24,12 @@ def result():
         b = request.form["POIstate"]
         c = request.form["LANGstate"]
         d = request.form["Lstate"]
-        #e = request.form["Tstate"]
-        print('submitted')
         return redirect(url_for("result", a=a, b=b, c=c, d=d))
     else:
         filters = []
         topposts = []
         placeholder =''
+        testname = ''
     #SEARCH
         if(request.args['a'] ==""):
             srch = 'tweet_text: *'
@@ -44,10 +42,12 @@ def result():
         if(request.args['b'] =="Select"):
             poilimit = "-poi_name: null"
             poilimit1 = "-poi_name: none"
+            gpoi=''
             filters.append(poilimit)
             filters.append(poilimit1)
         else:
             poiname = "poi_name: " + request.args['b']
+            gpoi = request.args['b']
             filters.append(poiname)
     #LANGUAGE
         if(request.args['c'] =="Select"):
@@ -61,9 +61,10 @@ def result():
         else:
             country = "country: " + request.args['d']
         filters.append(country)
-
+        print("------Solr Query------")
         print(srch)
         print(filters)
+    #-------------pysolr search------------
         solr = pysolr.Solr('http://18.234.178.228:8983/solr/IRF20P4/')
         result = solr.search(srch, fq=filters, rows=20)
         num = result.raw_response['response']['numFound']
@@ -71,27 +72,54 @@ def result():
         if num<15:
             while x<num:
                 tweet=[]
-                tweet.append(result.raw_response['response']['docs'][x]['user.screen_name'])
-                tweet.append(result.raw_response['response']['docs'][x]['full_text'])
-                tweet.append(result.raw_response['response']['docs'][x]['country'])
-                tweet.append(result.raw_response['response']['docs'][x]['created_at'])
-                tweet.append(result.raw_response['response']['docs'][x]['retweet_count'])
-                tweet.append(result.raw_response['response']['docs'][x]['favorite_count'])
+                tweet.append(result.raw_response['response']['docs'][x]['user.screen_name'][0])
+                testname = result.raw_response['response']['docs'][x]['user.name'][0]
+                tweet.append(result.raw_response['response']['docs'][x]['full_text'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['country'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['created_at'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['retweet_count'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['favorite_count'][0])
                 topposts.append(tweet)
                 x+=1
         else:
             while x!=15:
                 tweet=[]
-                tweet.append(result.raw_response['response']['docs'][x]['user.screen_name'])
-                tweet.append(result.raw_response['response']['docs'][x]['full_text'])
-                tweet.append(result.raw_response['response']['docs'][x]['country'])
-                tweet.append(result.raw_response['response']['docs'][x]['created_at'])
-                tweet.append(result.raw_response['response']['docs'][x]['retweet_count'])
-                tweet.append(result.raw_response['response']['docs'][x]['favorite_count'])
+                tweet.append(result.raw_response['response']['docs'][x]['user.screen_name'][0])
+                testname = result.raw_response['response']['docs'][x]['user.name'][0]
+                tweet.append(result.raw_response['response']['docs'][x]['full_text'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['country'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['created_at'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['retweet_count'][0])
+                tweet.append(result.raw_response['response']['docs'][x]['favorite_count'][0])
                 topposts.append(tweet)
                 x+=1
-        print(num)
-        return render_template("result.html", num=num , top = topposts, quey = placeholder)
+
+    #-------------google search------------
+        topnews = []
+        gquery = "news " + placeholder + " " + testname
+        googlenews = GoogleNews()
+        googlenews.get_news(gquery)
+        results = googlenews.results()
+        print("------Google Query------")
+        print(gquery)
+        #print(results[0])
+        if len(results) == 0:
+            print("No search results")
+        else:
+            y=0
+            while y!=5:
+                news = []
+                news.append(results[y]['title'])
+                news.append(results[y]['desc'])
+                link = "https://" + results[y]['desc']
+                news.append(link)
+                news.append(results[y]['site'])
+                #print(results[y]['title'])
+                topnews.append(news)
+                y+=1
+
+
+        return render_template("result.html", num=num , top = topposts, quey = placeholder, news = topnews)
 
 @app.route('/insights')
 def insights():
